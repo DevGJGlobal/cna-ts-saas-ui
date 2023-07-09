0451ff4a45d18405d182fa19cf758df54dbf2e4f
Dev Kanchen <dev@juneits.com>
2023-07-09 15:40:18 +0530

    Reverted commented code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Link, LinkProps, SaasProvider } from '@saas-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
 //export default function App({ Component, pageProps }: AppProps) {
-//  return <SaasProvider><Component {...pageProps} /></SaasProvider>
+//  return <Component {...pageProps} />
 //}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <SaasProvider><Component {...pageProps} /></SaasProvider>
+}
