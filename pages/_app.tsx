8467520226c8f4495e5fd2cea99b4b638c92fef6
Dev Kanchen <dev@juneits.com>
2023-07-09 15:30:24 +0530

    cna-ts Final commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Link, LinkProps, SaasProvider } from '@saas-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+//export default function App({ Component, pageProps }: AppProps) {
+//  return <SaasProvider><Component {...pageProps} /></SaasProvider>
+//}
